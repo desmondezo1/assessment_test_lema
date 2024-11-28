7df01e0da1d5d17c1d36ffc77830132edd52b889
Desmondezo1 <desezo@gmail.com>
2024-11-28 07:44:43 +0000

    updated the port

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const app = require('./src/app');
 const sequelize = require('./src/config/database');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 sequelize.authenticate()
     .then(() => console.log('Database connected'))
